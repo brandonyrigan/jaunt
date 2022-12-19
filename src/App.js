f3659918bec1fe55d7e5a5cccc7d3b01c71fe3e5
brandon <brandon.yrigan@tlgcohort.com>
2022-12-19 08:50:37 -0800

    Created home and trip details container, installed packages, created flight component, remove unused files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import TripDetails from "./containers/TripDetails";
+import Home from "./containers/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Route path="/home" exact component={Home} />
+                <Route path="/tripdetails" exact component={TripDetails} />
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
---
+++ src/containers/TripDetails.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Flights from "./Flights";
+
+class TripDetails extends React.Component {
+    render() {
+        return (
+            <Flights/>
+        )
+    }
+}
+
+export default TripDetails;
