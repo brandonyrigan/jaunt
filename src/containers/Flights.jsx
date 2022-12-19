be2bcf077557c62587e86233ad880ae647242408
brandon <brandon.yrigan@tlgcohort.com>
2022-12-19 10:59:46 -0800

    updated flights

--- src/containers/Flights.jsx
+++ src/containers/Flights.jsx
@@ -29,10 +29,37 @@ class Flights extends React.Component {
         console.log(response);
         let flights = [];
         let flightDetails = {};
+        let flightsMap = new Map();
+        let tripCount = 0;
         for (let i = 0; i < 10; i++) {
             flightDetails.id = response.trips[i].id;
+            flightsMap.set(response.trips[i].id, i);
             flightDetails.legIds = response.trips[i].legIds;
+            flightsMap.set(response.trips[i].legIds[0], i);
+            flightsMap.set(response.trips[i].legIds[1], i);
             flights.push(flightDetails);
+        }
+
+        for (let fare of response.fares) {
+            if (tripCount == 10) {
+                tripCount = 0;
+                break;
+            }
+            if (flightsMap.has(fare.tripId)) {
+                let flightIndex = flightsMap.get(fare.tripId);
+                flights[flightIndex].flightDetails.price = fare.price.totalAmountUsd;
+                tripCount++;
+            }
+        }
+
+        for (let leg of response.legs) {
+            if (flightsMap.has(leg.id)) {
+                let flightIndex = flightsMap.get(leg.id);
+                flights[flightIndex].flightDetails.departureTime = leg.departureTime;
+                flights[flightIndex].flightDetails.arrivalTime = leg.arrivalTime;
+                flights[flightIndex].flightDetails.duration = leg.duration;
+                flights[flightIndex].flightDetails.stopoversCount = leg.stopoversCount;
+            }
         }
 
         this.setState(() => {
