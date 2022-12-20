1ca7be8e45bc9b5223c2cad35fde06d9e3550f31
brandon <brandon.yrigan@tlgcohort.com>
2022-12-20 15:42:44 -0800

    added photo and weather to trip card component,

--- index.js
+++ index.js
@@ -1,51 +1,80 @@
+import { getPhoto } from "./javascript/photos/photo.js";
+import { getCurrentWeather } from "./javascript/weather/weather.js";
+
 tripForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    console.log(event);
-    let cardContainer = document.createElement("div");
-    cardContainer.className = "card mb-3";
-    cardContainer.style.width = "80%";
-    cardContainer.innerHTML = `
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <img
-                        src="https://robohash.org/trip"
-                        class="img-fluid rounded-start"
-                        alt="..."
-                    />
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 id="tripNameCard" class="card-title">${tripName.value}</h5>
-                        <p class="card-text">
-                            <span id="tripFromCard">${fromLocation.value}</span> - <span id="tripToCard">${toLocation.value}</span>
-                        </p>
-                        <p class="card-text">
-                            <span id="tripStartDateCard">${fromDate.value}</span> - <span id="tripEndDateCard">${toDate.value}</span>
-                        </p>
-                        <a href="./tripdetails.html" class="btn btn-primary" target="_blank">See details</a>
-                    </div>
-                </div>
-            </div>
-    `;
+	event.preventDefault();
 
-    tripContainer.appendChild(cardContainer);
+	const getPhotoAttempts = [];
+	const getCurrentWeatherAttempts = [];
+	getPhotoAttempts.push(getPhoto("vacation"));
+	getCurrentWeatherAttempts.push(getCurrentWeather("Denver"));
 
-    tripContainer.addEventListener("click", (event) => {
-        if (event.target.innerText === "See details") {
-            let parent = event.target.parentElement;
-            let tripName = parent.children[0].innerText;
-            let fromLocation = parent.children[1].children[0].innerText;
-            let toLocation = parent.children[1].children[1].innerText;
-            let startDate = parent.children[2].children[0].innerText;
-            let endDate = parent.children[2].children[1].innerText;
+	Promise.all([getPhoto("vacation"), getCurrentWeather("Denver")])
+		.then((json) => {
+			let tripName = document.getElementById("tripName");
+			let fromLocation = document.getElementById("fromLocation");
+			let toLocation = document.getElementById("toLocation");
+			let fromDate = document.getElementById("fromDate");
+			let toDate = document.getElementById("toDate");
 
-            localStorage.setItem("tripName", tripName);
-            localStorage.setItem("fromLocation", fromLocation);
-            localStorage.setItem("toLocation", toLocation);
-            localStorage.setItem("startDate", startDate);
-            localStorage.setItem("endDate", endDate);
-        }
-    });
+			let cardContainer = document.createElement("div");
+			cardContainer.className = "card mb-3";
 
-    tripForm.reset();
+			cardContainer.innerHTML = `
+			        <div class="row g-0">
+			            <div class="col-md-4">
+			                <img
+			                    src="${json[0].urls.small}"
+			                    class="img-fluid rounded-start"
+			                    alt="${tripName.value}"
+                                width="200px"
+			                />
+			            </div>
+			            <div class="col-md-7">
+			                <div class="card-body">
+			                    <h5 id="tripNameCard" class="card-title">${tripName.value}</h5>
+			                    <p class="card-text">
+			                        <span id="tripFromCard">${fromLocation.value}</span> - <span id="tripToCard">${toLocation.value}</span>
+			                    </p>
+			                    <p class="card-text">
+			                        <span id="tripStartDateCard">${fromDate.value}</span> - <span id="tripEndDateCard">${toDate.value}</span>
+			                    </p>
+			                    <a href="./tripdetails.html" class="btn btn-primary" target="_blank">See details</a>
+			                </div>
+			            </div>
+                        <div class="col-md-1">
+                            <p class="card-text">
+                                <img src="http://openweathermap.org/img/w/${json[1].weather[0].icon}.png" alt="weather icon">
+			                    <span id="currentWeather">${json[1].main.temp}</span>
+			                </p>
+                        </div>
+			        </div>
+			`;
+
+			tripContainer.appendChild(cardContainer);
+
+			tripForm.reset();
+		})
+		.catch((err) => console.log(err));
+
+	getCurrentWeather("Denver")
+		.then((json) => {})
+		.catch((err) => console.log(err));
+
+	tripContainer.addEventListener("click", (event) => {
+		if (event.target.innerText === "See details") {
+			let parent = event.target.parentElement;
+			let tripName = parent.children[0].innerText;
+			let fromLocation = parent.children[1].children[0].innerText;
+			let toLocation = parent.children[1].children[1].innerText;
+			let startDate = parent.children[2].children[0].innerText;
+			let endDate = parent.children[2].children[1].innerText;
+
+			localStorage.setItem("tripName", tripName);
+			localStorage.setItem("fromLocation", fromLocation);
+			localStorage.setItem("toLocation", toLocation);
+			localStorage.setItem("startDate", startDate);
+			localStorage.setItem("endDate", endDate);
+		}
+	});
 });
--- javascript/weather/weather.js
+++ javascript/weather/weather.js
@@ -1,29 +1,38 @@
-API_KEY = "26d0216d2bc1472b67d4b3e69210bfed";
+const API_KEY = "26d0216d2bc1472b67d4b3e69210bfed";
 
 function getWeather(cityName) {
-    const urlToFetch = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&APPID=${API_KEY}&units=imperial`;
-    fetch(urlToFetch)
-        .then((response) => response.json())
-        .then((jsonResponse) => showWeather(jsonResponse));
+	const urlToFetch = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&APPID=${API_KEY}&units=imperial`;
+	fetch(urlToFetch)
+		.then((response) => response.json())
+		.then((jsonResponse) => showWeather(jsonResponse));
 }
 
 // show four day forecast in a single div img and temp for each day
 function showWeather(jsonResponse) {
-    const weatherDiv = document.getElementById("weather");
-    const weather = jsonResponse.list;
-    const weatherHTML = [];
-    for (let i = 0; i < weather.length; i += 8) {
-        weatherHTML.push(`
+	const weatherDiv = document.getElementById("weather");
+	const weather = jsonResponse.list;
+	const weatherHTML = [];
+	for (let i = 0; i < weather.length; i += 8) {
+		weatherHTML.push(`
       <div class="weather-day">
         <p>${weather[i].dt_txt.slice(5, 10)}</p>
         <img src="http://openweathermap.org/img/w/${
-            weather[i].weather[0].icon
-        }.png" alt="weather icon">
+			weather[i].weather[0].icon
+		}.png" alt="weather icon">
         <p>${weather[i].main.temp}°F</p>
       </div>
     `);
-    }
-    weatherDiv.innerHTML = weatherHTML.join("");
+	}
+	weatherDiv.innerHTML = weatherHTML.join("");
 }
 
-export { getWeather };
+function getCurrentWeather(cityName) {
+	const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${API_KEY}&units=imperial`;
+	return new Promise((resolve, reject) => {
+		fetch(apiUrl)
+			.then((res) => resolve(res.json()))
+			.catch((err) => reject(err));
+	});
+}
+
+export { getWeather, getCurrentWeather };
