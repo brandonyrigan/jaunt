8ce18d63bd3f022967a784641061460b1d0432cd
Henrymelendez <henryomarm@gmail.com>
2022-12-20 14:27:43 -0800

    added helper function for airport codes

--- javascript/attractions/attractions.js
+++ javascript/attractions/attractions.js
@@ -11,12 +11,17 @@ function getLongandLat(city) {
     fetch(`https://api.geoapify.com/v1/geocode/search?text=${city}&format=json&apiKey=${api_key}`, requestOptions)
     .then(response => response.json())
     .then(result => {
-        console.log(result.features[0].properties.lat);
-        console.log(result.features[0].properties.lon);
-        return [result.features[0].properties.lat, result.features[0].properties.lon];
-    })
-    
+        const long = result.results[0].lon;
+        const lat = result.results[0].lat;
+        console.log(long, lat);
+        getAttractions(long, lat);
+    }
+    )
+    .catch(error => console.log('error', error));
 }
+
+
+
         
 
 
@@ -24,30 +29,29 @@ function getLongandLat(city) {
 
 
 
-
+// return a list of attractions names, lat, long, and address for a given city
   function getAttractions(long, lat){
-  const url =  `https://api.geoapify.com/v2/places?categories=tourism&filter=circle:${long},${lat},5000&bias=proximity:${long},${lat}&limit=20&apiKey=${api_key}`;
-     try {
-         const response =  fetch(url);
-         if (response.ok) {
-             const jsonResponse = response.json();
-             const attractions = jsonResponse.features;
-             const attractionsArray = attractions.map(attraction => {
-                 return {
-                     name: attraction.properties.name,
-                     city: attraction.properties.city,
-                     image: attraction.properties.image,
-                     description: attraction.properties.description,
-                     link: attraction.properties.wikipedia
-                 }
-             });
-             return attractionsArray;
-         }
-     }
-     catch (error) {
-         console.log(error);
-     }
- }
+    fetch(`https://api.geoapify.com/v2/places?categories=tourism&filter=circle:${long},${lat},5000&bias=proximity:${long},${lat}&limit=20&apiKey=${api_key}`)
+    .then(response => response.json())
+    .then(result => {
+        console.log(result.features);
+        const attractions = result.features.map(attraction => {
+            return {
+                name: attraction.properties.name,
+                lat: attraction.properties.lat,
+                long: attraction.properties.lon,
+                address: attraction.properties.formatted_address
+            }
+        })
+        console.log(attractions);
+        return attractions;
+    })
+    .catch(error => console.log('error', error));
+    }
 
 
-console.log(getLongandLat('New York'));
+
+
+
+const c = getLongandLat('Seattle');
+console.log(c);
